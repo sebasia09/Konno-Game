1b5db336c18bfb5a1c2bebd2a40fa09845f80491
sebasia09 <85451240+sebasia09@users.noreply.github.com>
2021-08-13 15:01:54 -0300

    Auto-save

--- scripts/engine.js
+++ scripts/engine.js
@@ -2,3 +2,29 @@ var atual = {                  // Objetos que definem todas variaveis atuais par
   radioMessage : "[11224] Sinal SOS recebido : 'ERR0R'",
   radioSound : "RADIO_SIGNAL"
 }
+
+var character = {
+  gotRadio : false,
+  gotLanterna : true,
+  gotMapa : false,
+  oxygen : 100 + "%"
+}
+
+
+function saveLocal(){
+  alert('jogo salvo')
+  localStorage.setItem("character", JSON.stringify(character))
+}
+
+function getGameData() {
+  character = JSON.parse(localStorage.getItem("character"))
+}
+
+if(localStorage.getItem("character")=== null){
+  saveLocal()
+  setInterval(saveLocal, 60000)
+} else {
+  getGameData()
+  setInterval(saveLocal, 60000)
+}
+
