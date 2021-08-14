7764ecf4ec07bdbb00ca3d786d9f3f11622dc4be
sebasia09 <85451240+sebasia09@users.noreply.github.com>
2021-08-14 00:41:53 -0300

    Update engine.js

--- scripts/engine.js
+++ scripts/engine.js
@@ -5,26 +5,40 @@ var atual = {                  // Objetos que definem todas variaveis atuais par
 
 var character = {
   gotRadio : false,
+  gotRadio2 : false,
   gotLanterna : true,
   gotMapa : false,
   oxygen : 100 + "%"
+
 }
+var gameTextAtual= ["intro"]
+
 
 
 function saveLocal(){
-  alert('jogo salvo')
+  console.log("JOGO SALVO")
   localStorage.setItem("character", JSON.stringify(character))
+  localStorage.setItem("gameTextAtual", JSON.stringify(gameTextAtual))
 }
 
 function getGameData() {
   character = JSON.parse(localStorage.getItem("character"))
+  gameTextAtual = JSON.parse(localStorage.getItem("gameTextAtual"))
 }
 
 if(localStorage.getItem("character")=== null){
   saveLocal()
-  setInterval(saveLocal, 60000)
+  setInterval(saveLocal, 300000)
 } else {
   getGameData()
-  setInterval(saveLocal, 60000)
+  setInterval(saveLocal, 300000)
 }
 
+
+const gameText = {
+  intro : "Você acorda, sua cabeça dói.",
+  gotRadio : "Um rádio velho imundo esculhambado.",
+  gotRadio2 : "Talvez alguma peça esteja faltando."
+  
+
+}
